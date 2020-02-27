1e314c285d6119a352d8533e9c172a98ac30fcff
Wilson N <wilson.nger@live.com>
2020-02-27 22:51:30 +0800

    Replace model download source

--- python/python/jetson/inference/__init__.py
+++ python/python/jetson/inference/__init__.py
@@ -1,5 +1,5 @@
 
-print("jetson.inference.__init__.py")
+print("Jetson.Inference.__init__.py")
 
 from jetson_inference_python import *
 
--- tools/download-models.sh
+++ tools/download-models.sh
@@ -79,9 +79,9 @@ function attempt_download_file()
 {
 	local filename=$1
 	local URL=$2
-	
+
 	wget $WGET_QUIET --show-progress --progress=bar:force:noscroll --no-check-certificate $URL -O $filename
-	
+
 	local wget_status=$?
 
 	if [ $wget_status != 0 ]; then
@@ -101,7 +101,7 @@ function download_file()
 {
 	local filename=$1
 	local URL=$2
-	
+
 	WGET_QUIET="--quiet"
 
 	while true; do
@@ -114,7 +114,7 @@ function download_file()
 		fi
 
 		retry_prompt $filename $download_status
-	
+
 		local retry_status=$?
 
 		if [ $retry_status != 0 ]; then
@@ -131,9 +131,9 @@ function attempt_download_archive()
 {
 	local filename=$1
 	local URL=$2
-	
+
 	wget $WGET_QUIET --show-progress --progress=bar:force:noscroll --no-check-certificate $URL -O $filename
-	
+
 	local wget_status=$?
 
 	if [ $wget_status != 0 ]; then
@@ -162,7 +162,7 @@ function download_archive()
 {
 	local filename=$1
 	local URL=$2
-	
+
 	WGET_QUIET="--quiet"
 
 	while true; do
@@ -175,7 +175,7 @@ function download_archive()
 		fi
 
 		retry_prompt $filename $download_status
-	
+
 		local retry_status=$?
 
 		if [ $retry_status != 0 ]; then
@@ -191,66 +191,66 @@ function download_alexnet()
 {
 	echo "$LOG Downloading AlexNet..."
 
-	download_file "bvlc_alexnet.caffemodel" "https://nvidia.box.com/shared/static/5j264j7mky11q8emy4q14w3r8hl5v6zh.caffemodel"
-	download_file "alexnet.prototxt" "https://nvidia.box.com/shared/static/c84wp3axbtv4e2gybn40jprdquav9azm.prototxt"
-	download_file "alexnet_noprob.prototxt" "https://nvidia.box.com/shared/static/o0w0sl3obqxj21u09c0cwzw4khymz7hh.prototxt"
+	download_file "bvlc_alexnet.caffemodel" "https://d.nvidia-china.com/models/bvlc_alexnet.caffemodel"
+	download_file "alexnet.prototxt" "https://d.nvidia-china.com/models/alexnet.prototxt"
+	download_file "alexnet_noprob.prototxt" "https://d.nvidia-china.com/models/alexnet_noprob.prototxt"
 }
 
 function download_googlenet()
 {
 	echo "$LOG Downloading GoogleNet..."
 
-	download_file "bvlc_googlenet.caffemodel" "https://nvidia.box.com/shared/static/at8b1105ww1c5h7p30j5ko8qfnxrs0eg.caffemodel" 
-	download_file "googlenet.prototxt" "https://nvidia.box.com/shared/static/5z3l76p8ap4n0o6rk7lyasdog9f14gc7.prototxt"
-	download_file "googlenet_noprob.prototxt" "https://nvidia.box.com/shared/static/ue8qrqtglu36andbvobvaaj8egxjaoli.prototxt"
+	download_file "bvlc_googlenet.caffemodel" "https://d.nvidia-china.com/models/bvlc_googlenet.caffemodel"
+	download_file "googlenet.prototxt" "https://d.nvidia-china.com/models/googlenet.prototxt"
+	download_file "googlenet_noprob.prototxt" "https://d.nvidia-china.com/models/googlenet_noprob.prototxt"
 }
 
 function download_googlenet12()
 {
 	echo "$LOG Downloading GoogleNet-12..."
-	download_archive "GoogleNet-ILSVRC12-subset.tar.gz" "https://nvidia.box.com/shared/static/zb8i3zcg39sdjjxfty7o5935hpbd64y4.gz" 
+	download_archive "GoogleNet-ILSVRC12-subset.tar.gz" "https://d.nvidia-china.com/models/zb8i3zcg39sdjjxfty7o5935hpbd64y4.gz"
 }
 
 function download_resnet18()
 {
 	echo "$LOG Downloading ResNet-18..."
-	download_archive "ResNet-18.tar.gz" "https://nvidia.box.com/shared/static/gph1qfor89vh498op8cicvwc13zltu3h.gz" 
+	download_archive "ResNet-18.tar.gz" "https://d.nvidia-china.com/models/ResNet-18.tar.gz"
 }
 
 function download_resnet50()
 {
 	echo "$LOG Downloading ResNet-50..."
-	download_archive "ResNet-50.tar.gz" "https://nvidia.box.com/shared/static/ht46fmnwvow0o0n0ke92x6bzkht8g5xb.gz" 
+	download_archive "ResNet-50.tar.gz" "https://d.nvidia-china.com/models/ResNet-50.tar.gz"
 }
 
 function download_resnet101()
 {
 	echo "$LOG Downloading ResNet-101..."
-	download_archive "ResNet-101.tar.gz" "https://nvidia.box.com/shared/static/7zog25pu70nxjh2irni49e5ujlg4dl82.gz" 
+	download_archive "ResNet-101.tar.gz" "https://d.nvidia-china.com/models/ResNet-101.tar.gz"
 }
 
 function download_resnet152()
 {
 	echo "$LOG Downloading ResNet-152..."
-	download_archive "ResNet-152.tar.gz" "https://nvidia.box.com/shared/static/6t621ru1i054vscvhx3rqck8597es7w8.gz" 
+	download_archive "ResNet-152.tar.gz" "https://d.nvidia-china.com/models/ResNet-152.tar.gz"
 }
 
 function download_vgg16()
 {
 	echo "$LOG Downloading VGG-16..."
-	download_archive "VGG-16.tar.gz" "https://nvidia.box.com/shared/static/ar2ttdpnw1drzxnvpw0umzkw67fka3h0.gz" 
+	download_archive "VGG-16.tar.gz" "https://d.nvidia-china.com/models/VGG-16.tar.gz"
 }
 
 function download_vgg19()
 {
 	echo "$LOG Downloading VGG-19..."
-	download_archive "VGG-19.tar.gz" "https://nvidia.box.com/shared/static/1ubk73f1akhh4h7mo0iq7erars7j5yyu.gz" 
+	download_archive "VGG-19.tar.gz" "https://d.nvidia-china.com/models/VGG-19.tar.gz"
 }
 
 function download_inception_v4()
 {
 	echo "$LOG Downloading Inception-v4..."
-	download_archive "Inception-v4.tar.gz" "https://nvidia.box.com/shared/static/maidbjiwkg6bz2bk7drwq7rj8v4whdl9.gz" 
+	download_archive "Inception-v4.tar.gz" "https://d.nvidia-china.com/models/Inception-v4.tar.gz"
 }
 
 function download_recognition()
@@ -282,61 +282,61 @@ function download_recognition()
 function download_pednet()
 {
 	echo "$LOG Downloading PedNet..."
-	download_archive "ped-100.tar.gz" "https://nvidia.box.com/shared/static/0wbxo6lmxfamm1dk90l8uewmmbpbcffb.gz" 
+	download_archive "ped-100.tar.gz" "https://d.nvidia-china.com/models/ped-100.tar.gz"
 }
 
 function download_multiped()
 {
 	echo "$LOG Downloading MultiPed..."
-	download_archive "multiped-500.tar.gz" "https://nvidia.box.com/shared/static/r3bq08qh7zb0ap2lf4ysjujdx64j8ofw.gz" 
+	download_archive "multiped-500.tar.gz" "https://d.nvidia-china.com/models/multiped-500.tar.gz"
 }
 
 function download_facenet()
 {
 	echo "$LOG Downloading FaceNet..."
-	download_archive "facenet-120.tar.gz" "https://nvidia.box.com/shared/static/wjitc00ef8j6shjilffibm6r2xxcpigz.gz" 
+	download_archive "facenet-120.tar.gz" "https://d.nvidia-china.com/models/facenet-120.tar.gz"
 }
 
 function download_detectnet_coco_dog()
 {
 	echo "$LOG Downloading DetectNet-COCO-Dog..."
-	download_archive "DetectNet-COCO-Dog.tar.gz" "https://nvidia.box.com/shared/static/3qdg3z5qvl8iwjlds6bw7bwi2laloytu.gz" 
+	download_archive "DetectNet-COCO-Dog.tar.gz" "https://d.nvidia-china.com/models/DetectNet-COCO-Dog.tar.gz"
 }
 
 function download_detectnet_coco_chair()
 {
 	echo "$LOG Downloading DetectNet-COCO-Chair..."
-	download_archive "DetectNet-COCO-Chair.tar.gz" "https://nvidia.box.com/shared/static/fq0m0en5mmssiizhs9nxw3xtwgnoltf2.gz" 
+	download_archive "DetectNet-COCO-Chair.tar.gz" "https://d.nvidia-china.com/models/DetectNet-COCO-Chair.tar.gz"
 }
 
 function download_detectnet_coco_bottle()
 {
 	echo "$LOG Downloading DetectNet-COCO-Bottle..."
-	download_archive "DetectNet-COCO-Bottle.tar.gz" "https://nvidia.box.com/shared/static/8bhm91o9yldpf97dcz5d0welgmjy7ucw.gz" 
+	download_archive "DetectNet-COCO-Bottle.tar.gz" "https://d.nvidia-china.com/models/DetectNet-COCO-Bottle.tar.gz"
 }
 
 function download_detectnet_coco_airplane()
 {
 	echo "$LOG Downloading DetectNet-COCO-Airplane..."
-	download_archive "DetectNet-COCO-Airplane.tar.gz" "https://nvidia.box.com/shared/static/xi71hlsht5b0y66loeg73rxfa73q561s.gz" 
+	download_archive "DetectNet-COCO-Airplane.tar.gz" "https://d.nvidia-china.com/models/DetectNet-COCO-Airplane.tar.gz"
 }
 
 function download_ssd_mobilenet_v1()
 {
 	echo "$LOG Downloading SSD-Mobilenet-v1..."
-	download_archive "SSD-Mobilenet-v1.tar.gz" "https://nvidia.box.com/shared/static/0pg3xi9opwio65df14rdgrtw40ivbk1o.gz" 
+	download_archive "SSD-Mobilenet-v1.tar.gz" "https://d.nvidia-china.com/models/SSD-Mobilenet-v1.tar.gz"
 }
 
 function download_ssd_mobilenet_v2()
 {
 	echo "$LOG Downloading SSD-Mobilenet-v2..."
-	download_archive "SSD-Mobilenet-v2.tar.gz" "https://nvidia.box.com/shared/static/jcdewxep8vamzm71zajcovza938lygre.gz" 
+	download_archive "SSD-Mobilenet-v2.tar.gz" "https://d.nvidia-china.com/models/SSD-Mobilenet-v2.tar.gz"
 }
 
 function download_ssd_inception_v2()
 {
 	echo "$LOG Downloading SSD-Inception-v2..."
-	download_archive "SSD-Inception-v2.tar.gz" "https://nvidia.box.com/shared/static/mjq1cel6r5mdk94yb9o6v4nj8gxzlflr.gz" 
+	download_archive "SSD-Inception-v2.tar.gz" "https://d.nvidia-china.com/models/SSD-Inception-v2.tar.gz"
 }
 
 function download_detection()
@@ -366,67 +366,67 @@ function download_detection()
 function download_fcn_resnet18_cityscapes_512x256()
 {
 	echo "$LOG Downloading FCN-ResNet18-Cityscapes-512x256..."
-	download_archive "FCN-ResNet18-Cityscapes-512x256.tar.gz" "https://nvidia.box.com/shared/static/k7s7gdgi098309fndm2xbssj553vf71s.gz" 
+	download_archive "FCN-ResNet18-Cityscapes-512x256.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-Cityscapes-512x256.tar.gz"
 }
 
 function download_fcn_resnet18_cityscapes_1024x512()
 {
 	echo "$LOG Downloading FCN-ResNet18-Cityscapes-1024x512..."
-	download_archive "FCN-ResNet18-Cityscapes-1024x512.tar.gz" "https://nvidia.box.com/shared/static/9aqg4gpjmk7ipz4z0raa5mvs35om6emy.gz" 
+	download_archive "FCN-ResNet18-Cityscapes-1024x512.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-Cityscapes-1024x512.tar.gz"
 }
 
 function download_fcn_resnet18_cityscapes_2048x1024()
 {
 	echo "$LOG Downloading FCN-ResNet18-Cityscapes-2048x1024..."
-	download_archive "FCN-ResNet18-Cityscapes-2048x1024.tar.gz" "https://nvidia.box.com/shared/static/ylh3d2qk8qvitalq8sy803o7avrb6w0h.gz" 
+	download_archive "FCN-ResNet18-Cityscapes-2048x1024.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-Cityscapes-2048x1024.tar.gz"
 }
 
 function download_fcn_resnet18_deepscene_576x320()
 {
 	echo "$LOG Downloading FCN-ResNet18-DeepScene-576x320..."
-	download_archive "FCN-ResNet18-DeepScene-576x320.tar.gz" "https://nvidia.box.com/shared/static/jm0zlezvweiimpzluohg6453s0u0nvcv.gz" 
+	download_archive "FCN-ResNet18-DeepScene-576x320.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-DeepScene-576x320.tar.gz"
 }
 
 function download_fcn_resnet18_deepscene_864x480()
 {
 	echo "$LOG Downloading FCN-ResNet18-DeepScene-864x480..."
-	download_archive "FCN-ResNet18-DeepScene-864x480.tar.gz" "https://nvidia.box.com/shared/static/gooux9b5nknk8wlk60ou9s2unpo760iq.gz" 
+	download_archive "FCN-ResNet18-DeepScene-864x480.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-DeepScene-864x480.tar.gz"
 }
 
 function download_fcn_resnet18_mhp_512x320()
 {
 	echo "$LOG Downloading FCN-ResNet18-MHP-512x320..."
-	download_archive "FCN-ResNet18-MHP-512x320.tar.gz" "https://nvidia.box.com/shared/static/dgaw0ave3bdws1t5ed333ftx5dbpt9zv.gz" 
+	download_archive "FCN-ResNet18-MHP-512x320.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-MHP-512x320.tar.gz"
 }
 
 function download_fcn_resnet18_mhp_640x360()
 {
 	echo "$LOG Downloading FCN-ResNet18-MHP-640x360..."
-	download_archive "FCN-ResNet18-MHP-640x360.tar.gz" "https://nvidia.box.com/shared/static/50mvlrjwbq9ugkmnnqp1sm99g2j21sfn.gz" 
+	download_archive "FCN-ResNet18-MHP-640x360.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-MHP-640x360.tar.gz"
 }
 
 function download_fcn_resnet18_pascal_voc_320x320()
 {
 	echo "$LOG Downloading FCN-ResNet18-Pascal-VOC-320x320..."
-	download_archive "FCN-ResNet18-Pascal-VOC-320x320.tar.gz" "https://nvidia.box.com/shared/static/p63pgrr6tm33tn23913gq6qvaiarydaj.gz" 
+	download_archive "FCN-ResNet18-Pascal-VOC-320x320.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-Pascal-VOC-320x320.tar.gz"
 }
 
 function download_fcn_resnet18_pascal_voc_512x320()
 {
 	echo "$LOG Downloading FCN-ResNet18-Pascal-VOC-512x320..."
-	download_archive "FCN-ResNet18-Pascal-VOC-512x320.tar.gz" "https://nvidia.box.com/shared/static/njup7f3vu4mgju89kfre98olwljws5pk.gz" 
+	download_archive "FCN-ResNet18-Pascal-VOC-512x320.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-Pascal-VOC-512x320.tar.gz"
 }
 
 function download_fcn_resnet18_sun_rgbd_512x400()
 {
 	echo "$LOG Downloading FCN-ResNet18-SUN-RGBD-512x400..."
-	download_archive "FCN-ResNet18-SUN-RGBD-512x400.tar.gz" "https://nvidia.box.com/shared/static/5vs9t2wah5axav11k8o3l9skb7yy3xgd.gz" 
+	download_archive "FCN-ResNet18-SUN-RGBD-512x400.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-SUN-RGBD-512x400.tar.gz"
 }
 
 function download_fcn_resnet18_sun_rgbd_640x512()
 {
 	echo "$LOG Downloading FCN-ResNet18-SUN-RGBD-640x512..."
-	download_archive "FCN-ResNet18-SUN-RGBD-640x512.tar.gz" "https://nvidia.box.com/shared/static/z5llxysbcqd8zzzsm7vjqeihs7ihdw20.gz" 
+	download_archive "FCN-ResNet18-SUN-RGBD-640x512.tar.gz" "https://d.nvidia-china.com/models/FCN-ResNet18-SUN-RGBD-640x512.tar.gz"
 }
 
 function download_segmentation()
@@ -444,7 +444,7 @@ function download_segmentation()
 	download_fcn_resnet18_pascal_voc_512x320
 	download_fcn_resnet18_sun_rgbd_512x400
 	download_fcn_resnet18_sun_rgbd_640x512
-	
+
 	ALL_SEGMENTATION=1
 }
 
@@ -455,43 +455,43 @@ function download_segmentation()
 function download_fcn_alexnet_cityscapes_sd()
 {
 	echo "$LOG Downloading FCN-Alexnet-Cityscapes-SD..."
-	download_archive "FCN-Alexnet-Cityscapes-SD.tar.gz" "https://nvidia.box.com/shared/static/pa5d338t9ntca5chfbymnur53aykhall.gz" 
+	download_archive "FCN-Alexnet-Cityscapes-SD.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-Cityscapes-SD.tar.gz"
 }
 
 function download_fcn_alexnet_cityscapes_hd()
 {
 	echo "$LOG Downloading FCN-Alexnet-Cityscapes-HD..."
-	download_archive "FCN-Alexnet-Cityscapes-HD.tar.gz" "https://nvidia.box.com/shared/static/mh121fvmveemujut7d8c9cbmglq18vz3.gz" 
+	download_archive "FCN-Alexnet-Cityscapes-HD.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-Cityscapes-HD.tar.gz"
 }
 
 function download_fcn_alexnet_aerial_fpv()
 {
 	echo "$LOG Downloading FCN-Alexnet-Aerial-FPV..."
-	download_archive "FCN-Alexnet-Aerial-FPV-720p.tar.gz" "https://nvidia.box.com/shared/static/y1mzlwkmytzwg2m7akt7tcbsd33f9opz.gz" 
+	download_archive "FCN-Alexnet-Aerial-FPV-720p.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-Aerial-FPV-720p.tar.gz"
 }
 
 function download_fcn_alexnet_pascal_voc()
 {
 	echo "$LOG Downloading FCN-Alexnet-Pascal-VOC..."
-	download_archive "FCN-Alexnet-Pascal-VOC.tar.gz" "https://nvidia.box.com/shared/static/xj20b6qopfwkkpqm12ffiuaekk6bs8op.gz" 
+	download_archive "FCN-Alexnet-Pascal-VOC.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-Pascal-VOC.tar.gz"
 }
 
 function download_fcn_alexnet_synthia_cvpr()
 {
 	echo "$LOG Downloading FCN-Alexnet-Synthia-CVPR..."
-	download_archive "FCN-Alexnet-SYNTHIA-CVPR16.tar.gz" "https://nvidia.box.com/shared/static/u5ey2ws0nbtzyqyftkuqazx1honw6wry.gz" 
+	download_archive "FCN-Alexnet-SYNTHIA-CVPR16.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-SYNTHIA-CVPR16.tar.gz"
 }
 
 function download_fcn_alexnet_synthia_summer_sd()
 {
 	echo "$LOG Downloading FCN-Alexnet-Synthia-Summer-SD..."
-	download_archive "FCN-Alexnet-SYNTHIA-Summer-SD.tar.gz" "https://nvidia.box.com/shared/static/vbk5ofu1x2hwp9luanbg4o0vrfub3a7j.gz" 
+	download_archive "FCN-Alexnet-SYNTHIA-Summer-SD.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-SYNTHIA-Summer-SD.tar.gz"
 }
 
 function download_fcn_alexnet_synthia_summer_hd()
 {
 	echo "$LOG Downloading FCN-Alexnet-Synthia-Summer-HD..."
-	download_archive "FCN-Alexnet-SYNTHIA-Summer-HD.tar.gz" "https://nvidia.box.com/shared/static/ydgmqgdhbvul6q9avoc9flxr3fdoa8pw.gz" 
+	download_archive "FCN-Alexnet-SYNTHIA-Summer-HD.tar.gz" "https://d.nvidia-china.com/models/FCN-Alexnet-SYNTHIA-Summer-HD.tar.gz"
 }
 
 function download_segmentation_legacy()
@@ -516,13 +516,13 @@ function download_segmentation_legacy()
 function download_deep_homography_coco()
 {
 	echo "$LOG Downloading Deep-Homography-COCO..."
-	download_archive "Deep-Homography-COCO.tar.gz" "https://nvidia.box.com/shared/static/nlqbsdnt76y0nmkwdzxkg4zbvhk4bidh.gz" 
+	download_archive "Deep-Homography-COCO.tar.gz" "https://d.nvidia-china.com/models/Deep-Homography-COCO.tar.gz"
 }
 
 function download_super_resolution_bsd500()
 {
 	echo "$LOG Downloading Super-Resolution-BSD500..."
-	download_archive "Super-Resolution-BSD500.tar.gz" "https://nvidia.box.com/shared/static/a99l8ttk21p3tubjbyhfn4gh37o45rn8.gz" 
+	download_archive "Super-Resolution-BSD500.tar.gz" "https://d.nvidia-china.com/models/Super-Resolution-BSD500.tar.gz"
 }
 
 function download_image_processing()
@@ -563,7 +563,7 @@ function find_deb_package()
 function install_deb_package()
 {
 	local PKG_NAME=$1
-	
+
 	# check to see if the package is already installed
 	find_deb_package $PKG_NAME $2
 
@@ -574,10 +574,10 @@ function install_deb_package()
 	else
 		return 0
 	fi
-	
+
 	# verify that the package was installed
 	find_deb_package $PKG_NAME $2
-	
+
 	if [ -z $2 ]; then
 		echo "$LOG Failed to install '$PKG_NAME' deb package."
 		return 1
@@ -608,7 +608,7 @@ while true; do
 							  --checklist "Keys:\n  ↑↓  Navigate Menu\n  Space to Select Models \n  Enter to Continue" 20 80 10 \
 							  --output-fd 1 \
 							  1 "\ZbImage Recognition - all models  (2.2 GB)\Zn" off \
-							  2 "   > AlexNet                    (244 MB)" off \
+							  2 "   > AlexNet                    (244 MB)" on \
 							  3 "   > GoogleNet                  (54 MB)" on \
 							  4 "   > GoogleNet-12               (42 MB)" off \
 							  5 "   > ResNet-18                  (47 MB)" on \
@@ -620,26 +620,26 @@ while true; do
 							  11 "   > Inception-v4               (172 MB)" off \
 							  12 "\ZbObject Detection - all models   (395 MB)\Zn" off \
 							  13 "   > SSD-Mobilenet-v1           (27 MB)" off \
-							  14 "   > SSD-Mobilenet-v2           (68 MB)" on \
+							  14 "   > SSD-Mobilenet-v2           (68 MB)" off \
 							  15 "   > SSD-Inception-v2           (100 MB)" off \
-							  16 "   > PedNet                     (30 MB)" on \
+							  16 "   > PedNet                     (30 MB)" off \
 							  17 "   > MultiPed                   (30 MB)" off \
-							  18 "   > FaceNet                    (24 MB)" on \
-							  19 "   > DetectNet-COCO-Dog         (29 MB)" on \
+							  18 "   > FaceNet                    (24 MB)" off \
+							  19 "   > DetectNet-COCO-Dog         (29 MB)" off \
 							  20 "   > DetectNet-COCO-Bottle      (29 MB)" off \
 							  21 "   > DetectNet-COCO-Chair       (29 MB)" off \
 							  22 "   > DetectNet-COCO-Airplane    (29 MB)" off \
 							  23 "\ZbSemantic Segmentation - all            (518 MB)\Zn" off \
-							  24 "   > FCN-ResNet18-Cityscapes-512x256   (47 MB)" on \
-							  25 "   > FCN-ResNet18-Cityscapes-1024x512  (47 MB)" on \
+							  24 "   > FCN-ResNet18-Cityscapes-512x256   (47 MB)" off \
+							  25 "   > FCN-ResNet18-Cityscapes-1024x512  (47 MB)" off \
 							  26 "   > FCN-ResNet18-Cityscapes-2048x1024 (47 MB)" off \
-							  27 "   > FCN-ResNet18-DeepScene-576x320    (47 MB)" on \
+							  27 "   > FCN-ResNet18-DeepScene-576x320    (47 MB)" off \
 							  28 "   > FCN-ResNet18-DeepScene-864x480    (47 MB)" off \
-							  29 "   > FCN-ResNet18-MHP-512x320          (47 MB)" on \
+							  29 "   > FCN-ResNet18-MHP-512x320          (47 MB)" off \
 							  30 "   > FCN-ResNet18-MHP-640x360          (47 MB)" off \
-							  31 "   > FCN-ResNet18-Pascal-VOC-320x320   (47 MB)" on \
+							  31 "   > FCN-ResNet18-Pascal-VOC-320x320   (47 MB)" off \
 							  32 "   > FCN-ResNet18-Pascal-VOC-512x320   (47 MB)" off \
-							  33 "   > FCN-ResNet18-SUN-RGBD-512x400     (47 MB)" on \
+							  33 "   > FCN-ResNet18-SUN-RGBD-512x400     (47 MB)" off \
 							  34 "   > FCN-ResNet18-SUN-RGBD-640x512     (47 MB)" off \
 							  35 "\ZbSemantic Segmentation - legacy     (1.4 GB)\Zn" off \
 							  36 "   > FCN-Alexnet-Cityscapes-SD     (235 MB)" off \
@@ -664,7 +664,7 @@ while true; do
 			echo "$LOG No models were selected for download."
 		else
 			echo "$LOG Models selected for download:  $models_selected"
-		
+
 			for model in $models_selected
 			do
 				if [ $model = 1 ]; then
@@ -769,4 +769,3 @@ while true; do
 
 	exit_message 0
 done
-
